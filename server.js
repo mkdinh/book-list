afad9e51f032abf136c6f53e5dea15f5c8d4f528
Michael Dinh <mkdinh94@gmail.com>
2017-10-13 15:57:24 -0400

    fix index path

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Serve up static assets
-app.use(express.static(path.join(__dirname,"./client/build")));
+app.use(express.static("client/build"));
 // Add routes, both API and view
 app.use(routes);
 
