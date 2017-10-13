19f619ce5bb6672ea20b262bfcf4ec3879a8dac9
Michael Dinh <mkdinh94@gmail.com>
2017-10-13 15:47:09 -0400

    add extra )

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Serve up static assets
-app.use(express.static(path.join(__dirname,"./client/build"));
+app.use(express.static(path.join(__dirname,"./client/build")));
 // Add routes, both API and view
 app.use(routes);
 
